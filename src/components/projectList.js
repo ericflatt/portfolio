96c3a2d84ad2857ba0e1c79c2427c315ed1317d7
eric-blackmath <eric@blackmath.com>
2021-06-16 09:52:30 -0400

    font updates

---
+++ src/components/projectList.js
@@ -0,0 +1,61 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import "../styles/main.scss"
+
+class List extends React.Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div className="">
+        <div className="work">
+          <div className="work-item">
+            <div className="work-title work-item-inner">
+            <div className="work-sub-title">
+                <p>Senior Capstone</p>
+              </div>
+              <Link to="/page-2">Holo-cloud</Link>
+            </div>
+          </div>
+
+          <div className="work-item">
+            <div className="work-title work-item-inner">
+            <div className="work-sub-title">
+                <p>Human-Computer Interaction</p>
+              </div>
+              <Link to="/page-2">Obo</Link>
+            </div>
+          </div>
+
+          <div className="work-item">
+            <div className="work-title work-item-inner">
+              <div className="work-sub-title">
+                <p>Prototyping Electronics</p>
+              </div>
+              <Link to="/page-2">Sway</Link>
+            </div>
+          </div>
+
+          <div className="work-item">
+            <div className="work-title work-item-inner">
+            <div className="work-sub-title">
+                <p>Human Factors</p>
+              </div>
+              <Link to="/page-2">Bond</Link>
+            </div>
+          </div>
+
+          <div className="work-item">
+            <div className="work-title work-item-inner">
+              <Link to="/page-2">Floatie</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default List
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,15 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import Seo from "../components/seo"
+import List from "../components/projectList"
 import "../styles/main.scss"
 
 const IndexPage = () => (
 <main>
 <Seo title="Home" />
-    <h1>Hi</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </p>
+    <List />
+      
+   
 </main>
 
 )
