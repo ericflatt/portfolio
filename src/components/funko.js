bdb4fd662debbb27b0179b7d1283b879dff84590
ericflatt <eflatt18@gmail.com>
2023-02-23 15:14:18 -0800

    fixing

--- src/components/funko.js
+++ src/components/funko.js
@@ -5,7 +5,7 @@ import "../styles/playground.scss"
 import Seo from "../components/seo"
 import Cursor from "../components/cursor"
 import NavButtons from "./navButtons"
-import funko from "../images/mepop.png"
+// import funko from "../images/mepop.png"
 
 
 class Funko extends React.Component {
@@ -23,7 +23,7 @@ class Funko extends React.Component {
             Currently I am a UX/UI Designer on the digital experience team at
             Funko.
           </h2>
-          <img style={{width:"20%",marginTop:"24px"}} src={funko} alt="me as a funko" />
+          {/* <img style={{width:"20%",marginTop:"24px"}} src={funko} alt="me as a funko" /> */}
         </div>
       </div>
     )
