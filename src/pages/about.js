ec32dce0bf78ce96e75991496da384b43b2dce24
ericflatt <eflatt18@gmail.com>
2023-02-24 15:54:56 -0800

    nav button updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,14 +4,17 @@ import * as React from "react"
 import Seo from "../components/seo"
 import About from "../components/aboutPage"
 import "../styles/main.scss"
-import NavButtons from "../components/navButtons"
+// import NavButtons from "../components/navButtons"
 import MobileNav from "../components/mobilenav"
+import HomeNav from "../components/homenav"
+
+
 
 const IndexPage = () => (
   <main>
     <Seo title="About" />
     <About />
-    <NavButtons />
+    <HomeNav />
     <MobileNav />
   </main>
 )
